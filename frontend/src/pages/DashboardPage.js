cda57c42a8fcd9a98bf84a4db5ad4f27ba3f46b5
dotsond873 <dotsond873@gmail.com>
2026-06-13 00:22:40 -0500

    Update DashboardPage.js

--- frontend/src/pages/DashboardPage.js
+++ frontend/src/pages/DashboardPage.js
@@ -12,8 +12,7 @@ export default function DashboardPage({ user: propUser }) {
   const [nearbyMembers, setNearbyMembers] = useState([]);
 
   useEffect(() => {
-     axios.get(`${API}/auth/me`, { withCredentials: true })
-        .then(res => setUser(res.data))
+     axios.get(`${API}/auth/me`, { withCredentials: true })   .then(res => setUser(res.data))
         .catch(() => navigate('/login'));
     }
 
